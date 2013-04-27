d611527e08863229d8b679ed8671ae1454162e96
Ben Heebner <ben.heebner@gmail.com>
2013-04-27 14:45:51 -0700

    Initial Commit
    Fully working desktop site.  Mobile still needs work

---
+++ js/script.js
@@ -0,0 +1,197 @@
+/**
+ * Parallax Scrolling Tutorial
+ * For Smashing Magazine
+ * July 2011
+ *   
+ * Author: Richard Shepherd
+ * 		   www.richardshepherd.com
+ * 		   @richardshepherd   
+ */
+
+// On your marks, get set...
+$(document).ready(function(){
+						
+	// Cache the Window object
+	$window = $(window);
+	
+	// Cache the Y offset and the speed of each sprite
+	$('[data-type]').each(function() {	
+		$(this).data('offsetY', parseInt($(this).attr('data-offsetY')));
+		$(this).data('Xposition', $(this).attr('data-Xposition'));
+		$(this).data('speed', $(this).attr('data-speed'));
+	});
+	
+	// For each element that has a data-type attribute
+	$('section[data-type="background"]').each(function(){
+	
+	
+		// Store some variables based on where we are
+		var $self = $(this),
+			offsetCoords = $self.offset(),
+			topOffset = offsetCoords.top;
+		
+		// When the window is scrolled...
+	    $(window).scroll(function() {
+	
+			// If this section is in view
+			if ( ($window.scrollTop() + $window.height()) > (topOffset) &&
+				 ( (topOffset + $self.height()) > $window.scrollTop() ) ) {
+	
+				// Scroll the background at var speed
+				// the yPos is a negative value because we're scrolling it UP!								
+				var yPos = -($window.scrollTop() / $self.data('speed')); 
+				
+				// If this element has a Y offset then add it on
+				if ($self.data('offsetY')) {
+					yPos += $self.data('offsetY');
+				}
+				
+				// Put together our final background position
+				var coords = '50% '+ yPos + 'px';
+
+				// Move the background
+				$self.css({ backgroundPosition: coords });
+				
+				// Check for other sprites in this section	
+				$('[data-type="sprite"]', $self).each(function() {
+					
+					// Cache the sprite
+					var $sprite = $(this);
+					
+					// Use the same calculation to work out how far to scroll the sprite
+					var yPos = -($window.scrollTop() / $sprite.data('speed'));					
+					var coords = $sprite.data('Xposition') + ' ' + (yPos + $sprite.data('offsetY')) + 'px';
+					
+					$sprite.css({ backgroundPosition: coords });													
+					
+				}); // sprites
+			
+				// Check for any Videos that need scrolling
+				$('[data-type="video"]', $self).each(function() {
+					
+					// Cache the video
+					var $video = $(this);
+					
+					// There's some repetition going on here, so 
+					// feel free to tidy this section up. 
+					var yPos = -($window.scrollTop() / $video.data('speed'));					
+					var coords = (yPos + $video.data('offsetY')) + 'px';
+	
+					$video.css({ top: coords });													
+					
+				}); // video	
+			
+			}; // in view
+	
+		}); // window scroll
+			
+	});	// each data-type
+
+}); // document ready
+
+$(document).ready(init);
+
+function init() {
+
+	$(window).stellar();
+
+	/* ========== DRAWING THE PATH AND INITIATING THE PLUGIN ============= */
+
+	$.fn.scrollPath("getPath")
+		// Move to 'start' element
+		.moveTo(1000, 1000, {name: "start"})
+		// Line to 'description' element
+		.lineTo(400, 800, {name: "description"})
+		// Arc down and line to 'syntax'
+		.arc(200, 1200, 400, -Math.PI/2, Math.PI/2, true)
+		.lineTo(600, 1600, {
+			callback: function() {
+				highlight($(".settings"));
+			},
+			name: "syntax"
+		})
+		// Continue line to 'scrollbar'
+		.lineTo(1750, 1600, {
+			callback: function() {
+				highlight($(".sp-scroll-handle"));
+			},
+			name: "scrollbar"
+		})
+		// Arc up while rotating
+		.arc(1800, 1000, 600, Math.PI/2, 0, true, {rotate: Math.PI/2 })
+		// Line to 'rotations'
+		.lineTo(2400, 750, {
+			name: "rotations"
+		})
+		// Rotate in place
+		.rotate(3*Math.PI/2, {
+			name: "rotations-rotated"
+		})
+		// Continue upwards to 'source'
+		.lineTo(2400, -700, {
+			name: "source"
+		})
+		// Small arc downwards
+		.arc(2250, -700, 150, 0, -Math.PI/2, true)
+
+		//Line to 'follow'
+		.lineTo(1350, -850, {
+			name: "follow"
+		})
+		// Arc and rotate back to the beginning.
+		.arc(1300, 50, 900, -Math.PI/2, -Math.PI, true, {rotate: Math.PI*2, name: "end"});
+
+	// We're done with the path, let's initate the plugin on our wrapper element
+	$(".wrapper").scrollPath({drawPath: true, wrapAround: true});
+
+	// Add scrollTo on click on the navigation anchors
+	$("nav").find("a").each(function() {
+		var target = $(this).attr("href").replace("#", "");
+		$(this).click(function(e) {
+			e.preventDefault();
+			
+			// Include the jQuery easing plugin (http://gsgd.co.uk/sandbox/jquery/easing/)
+			// for extra easing functions like the one below
+			$.fn.scrollPath("scrollTo", target, 1000, "easeInOutSine");
+		});
+	});
+
+	/* ===================================================================== */
+
+	$(".settings .show-path").click(function(e) {
+		e.preventDefault();
+		$(".sp-canvas").toggle();
+	}).toggle(function() {
+		$(this).text("Hide Path");
+	}, function() {
+		$(this).text("Show Path");
+	});
+
+	$(".tweet").click(function(e) {
+		open(this.href, "", "width=550, height=450");
+		e.preventDefault();
+	});
+
+	$.getJSON("http://cdn.api.twitter.com/1/urls/count.json?callback=?&url=http%3A%2F%2Fjoelb.me%2Fscrollpath",
+			function(data) {
+				if(data && data.count !== undefined) {
+					$(".follow .count").html("the " + ordinal(data.count + 1) + " kind person to");
+				}
+			});
+	}
+
+
+function highlight(element) {
+	if(!element.hasClass("highlight")) {
+		element.addClass("highlight");
+		setTimeout(function() { element.removeClass("highlight"); }, 2000);
+	}
+}
+function ordinal(num) {
+	return num + (
+		(num % 10 == 1 && num % 100 != 11) ? 'st' :
+		(num % 10 == 2 && num % 100 != 12) ? 'nd' :
+		(num % 10 == 3 && num % 100 != 13) ? 'rd' : 'th'
+	);
+}
+
